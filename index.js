f0f44828532a614e72ad0fc724f7a61a1f55d9c9
Julian Webb <git@julianwebb.ca>
2021-08-01 19:27:35 -0700

    Added EditorConfig, Made everything 2spaces

--- index.js
+++ index.js
@@ -3,52 +3,103 @@ const { ThreadHandler } = require('./threadHandler');
 const { FeedEvents } = require('feed-events');
 
 class ParameterError extends Error {
-    constructor(...params) {
-        super(...params);
-    }
+  constructor(...params) {
+    super(...params);
+  }
 }
 
-function CheckParameter(parameter, errorMessage) {
-    if (!parameter) throw new ParameterError(errorMessage);
-    return parameter;
-}
+class FeedThreadsHandler extends EventEmitter {
+  /**
+   * @param {rssConfiguratiom} rss 
+   * @param {discordConfiguration} discord 
+   */
+  constructor(rss, discord) {
+    super();
+    this.rss = rss;
+    this.discord = discord;
 
-class FeedThreads extends EventEmitter {
-    constructor(rssConfig = {}, discordConfig = {}) {
-        super();
-        this.discord = {
-            version: discordConfig.version || 9,
-            authorization: CheckParameter(discordConfig.authorization || "Missing Discord Authorization"),
-            channel: CheckParameter(discordConfig.channel),
-            duration: discordConfig.duration || 60
-        }
-        let API = `https://discord.com/api/v` + this.discord.version;
-        this.thread = new ThreadHandler(API, this.discord.authorization);
+    this.thread = new ThreadHandler(
+      `https://discord.com/api/v` + this.discord.version, 
+      this.discord.authorization);
         
-        this.rss = {
-            URL: CheckParameter(rssConfig.URL, "Missing RSS URI"),
-            configuration: rssConfig.configuration || {},
-            latest: rssConfig.latest || null
-        }        
-    }
+  }
 
-    start() {
-        FeedEvents(this.rss.URL, this.rss.configuration, this.rss.latest)
-            .then(feed => {
-                feed.on("item", item => {
-                    this.emit("item", item, this.submit.bind(this));
-                });
-
+  /**
+   * Starts FeedThreads 
+   * @returns {Promise} 
+   */
+  async start() {
+    return FeedEvents(
+            this.rss.feedLocation,
+            this.rss.fetchSchedule,
+            this.rss.configuration, 
+            this.rss.latest)
+              .then(feed => {
+                feed.on("item", item => this.emit("item", item, this.submit.bind(this)));
                 feed.start();
-            })
-    }
+                this.feed = feed;
+              })
+  }
 
-    submit(title, message) {
-        this.thread.postToChannel(this.discord.channel, message)
-            .then(messageID => {
-                this.thread.createThreadOnMessage(this.discord.channel, messageID, title, this.discord.duration )
-            })
-    }
+
+  /**
+   * Submit a message object to predefined channel and create thread on it
+   * @param {string} threadTitle Title to use on the Discord Thread
+   * @param {Object} messageObject Discord Message Object
+   */
+  submit(threadTitle, messageObject) {
+    this.thread.postToChannel(this.discord.channel, messageObject)
+      .then(messageID => {
+        this.thread.createThreadOnMessage(
+          this.discord.channelID,
+          messageID,
+          threadTitle,
+          this.discord.autoArchiveDuration )
+      })
+  }
 }
 
-module.exports = FeedThreads;
+/**
+ * @typedef {Object} rssConfiguration
+ * @property {string} feedLocation URL of the RSS Feed
+ * @property {string} fetchSchedule Cron expression of fetch interval
+ * @property {Object} [configuration] Passthrough configuration for RSS-Parser
+ * @property {Object} [latest] Last item to have been previously proccessed
+ */
+
+/**
+ * @typedef {Object} discordConfiguration
+ * @property {string} authorization Authorization Token for the Discord API
+ * @property {string} channelID Snowflake for the channel in which to post messages
+ * @property {number} [autoArchiveDuration] Duration in minutes to automatically archive the thread after recent activity
+ * @property {number} [apiVersion] Version of the Discord API to use
+ */
+
+/**
+ * Create a FeedThreads Instance
+ * @param {rssConfiguration} rss 
+ * @param {discordConfiguration} discord 
+ * @returns {Promise<FeedThreadsHandler>} new FeedThreads instance
+ */
+async function FeedThreads(rss, discord) {
+  // Required parameters
+  if (!rss.feedLocation) throw new ParameterError("Missing Requred Parameter: rss.feedLocation");
+  if (!discord.authorization) throw new ParameterError("Missing Required Parameter: discord.authorization");
+  if (!discord.channelID) throw new ParameterError("Missing Required Parameter: discord.channelID");
+
+  // Optional parameters
+  rss.fetchSchedule = rss.fetchSchedule || "*/15 * * * *";
+  rss.configuration = rss.configuration || {};
+  rss.latest = rss.latest || {};
+  discord.apiVersion = discord.apiVersion || 9;
+  discord.autoArchiveDuration = discord.activeDuration || 60; // minutes
+
+  // Type checking
+  if (typeof rss.feedLocation != "string") throw new ParameterError(`Incorrect Parameter Type for 'rss.feedLocation': Expected 'string', got '${typeof rss.feedLocation}' instead.`);
+  if (typeof discord.authorization != "string")  throw new ParameterError(`Incorrect Parameter Type for 'discord.authorization': Expected 'string', got '${typeof discord.authorization}' instead.`);
+  if (typeof discord.channelID != "string")  throw new ParameterError(`Incorrect Parameter Type for 'discord.channelID': Expected 'string', got '${typeof discord.channelID}' instead.`);
+
+  return new FeedThreadsHandler(rss, discord);
+}
+
+module.exports = { FeedThreads };
--- threadHandler.js
+++ threadHandler.js
@@ -1,54 +1,54 @@
 class ThreadHandler {
-    constructor(apiBase, authorization) {
-        try {
-            this.axios = (require('axios')).default.create({
-                baseURL: apiBase,
-                headers : {
-                    common: {
-                        'Authorization': authorization,
-                        'User-Agent': 'FeedThreads (https://github.com/JulianWebb/feedthread, 0.1.0)'
-                    }
-                }
-            });
-        } catch (exception) {
-            console.error("Could not create axios instance, did you install the dependencies?", exception);
-            process.exit(1);
+  constructor(apiBase, authorization) {
+    try {
+      this.axios = (require('axios')).default.create({
+        baseURL: apiBase,
+        headers : {
+          common: {
+            'Authorization': authorization,
+            'User-Agent': 'FeedThreads (https://github.com/JulianWebb/feedthread, 0.1.0)'
+          }
         }
+      });
+    } catch (exception) {
+      console.error("Could not create axios instance, did you install the dependencies?", exception);
+      process.exit(1);
     }
+  }
 
-    postToChannel(channel, message) {
-        return new Promise((resolve, reject) => {
-            this.axios.post(`/channels/${channel}/messages`, message)
-                .then(response => {
-                console.log(response.status, `Sucessfully posted to channel (${channel})`)
-                resolve(response.data.id)
-            })  .catch(reason => {
-                console.error(reason.response.status, `Failed to post to channel (${channel})`)
-                console.error(JSON.stringify(reason.response.data))
-                reject(reason);
-            })
-        })
-    }
+  postToChannel(channel, message) {
+    return new Promise((resolve, reject) => {
+      this.axios.post(`/channels/${channel}/messages`, message)
+        .then(response => {
+        console.log(response.status, `Sucessfully posted to channel (${channel})`)
+        resolve(response.data.id)
+      })  .catch(reason => {
+        console.error(reason.response.status, `Failed to post to channel (${channel})`)
+        console.error(JSON.stringify(reason.response.data))
+        reject(reason);
+      })
+    })
+  }
 
-    createThreadOnMessage(channel, message, title, duration) {
-        this.axios.post(`/channels/${channel}/messages/${message}/threads`, {
-            name: title,
-            duration: duration
-        }).then(response => console.log(response.status, `Successfully created thread (${response.data.id})`))
-        .catch(reason => {
-            console.error(reason.response.status, `Failed to create thread`)
-            console.error(JSON.stringify(reason.response.data));
-        })
-    }
+  createThreadOnMessage(channel, message, title, duration) {
+    this.axios.post(`/channels/${channel}/messages/${message}/threads`, {
+      name: title,
+      duration: duration
+    }).then(response => console.log(response.status, `Successfully created thread (${response.data.id})`))
+    .catch(reason => {
+      console.error(reason.response.status, `Failed to create thread`)
+      console.error(JSON.stringify(reason.response.data));
+    })
+  }
 
-    crosspostMessage(channel, message) {
-        this.axios.post(`/channels/${channel}/messages/${message}/crosspost`)
-            .then(response => console.log(response.status, `Successfully crossposted message (${response.data.id})`))
-            .catch(reason => {
-                console.error(reason.response.status, `Failed to crosspost`)
-                console.error(JSON.stringify(reason.response.data));
-            })
-    }
+  crosspostMessage(channel, message) {
+    this.axios.post(`/channels/${channel}/messages/${message}/crosspost`)
+      .then(response => console.log(response.status, `Successfully crossposted message (${response.data.id})`))
+      .catch(reason => {
+        console.error(reason.response.status, `Failed to crosspost`)
+        console.error(JSON.stringify(reason.response.data));
+      })
+  }
 }
 
 module.exports = { ThreadHandler };
